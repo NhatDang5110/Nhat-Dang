47decd4b51e50c23d16c908144eb7abf46809a62
NhatDang5110 <chaunhatdangne5110@gmail.com>
2025-01-19 14:13:49 +0700

    nnn
    bnnjh

---
+++ Căn bậc.py
@@ -0,0 +1,9 @@
+import math
+
+x = float(input())
+
+can_bac_2 = x ** (1/2)
+can_bac_3 = x ** (1/3)
+
+print(f"Căn bậc 2 của {x} là:" '{:.2f}'.format(can_bac_2))
+print(f"Căn bậc 3 của {x} là:" '%.2f' % can_bac_3)
